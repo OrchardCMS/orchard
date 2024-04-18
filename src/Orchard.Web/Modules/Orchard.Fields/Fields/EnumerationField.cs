b5eebb150c05fb59af245644515e66f8b777ae67
Benedek Farkas <benedek.farkas@lombiq.com>
2024-04-18 17:46:16 +0200

    Fixing exception when creating new item with CheckboxList flavor, adding more nullchecks and compactness

--- src/Orchard.Web/Modules/Orchard.Fields/Fields/EnumerationField.cs
+++ src/Orchard.Web/Modules/Orchard.Fields/Fields/EnumerationField.cs
@@ -7,34 +7,16 @@ namespace Orchard.Fields.Fields {
         private const char Separator = ';';
 
         public string Value {
-            get { return Storage.Get<string>().Trim(new char[] { Separator }); }
-            set {
-                Storage.Set(string.IsNullOrEmpty(value)
-                    ? string.Empty
-                    // we add the Separator around the value here
-                    : Separator + value.Trim(new char[] { Separator }) + Separator);
-            }
+            get => Storage.Get<string>()?.Trim(Separator) ?? "";
+            set => Storage.Set(string.IsNullOrWhiteSpace(value)
+                ? string.Empty
+                // It is now the responsibility of this field to (re-)add the separators.
+                : Separator + value.Trim(Separator) + Separator);
         }
 
         public string[] SelectedValues {
-            get {
-                var value = Value;
-                if (string.IsNullOrWhiteSpace(value)) {
-                    return new string[0];
-                }
-
-                return value.Split(new[] { Separator }, StringSplitOptions.RemoveEmptyEntries);
-            }
-
-            set {
-                if (value == null || value.Length == 0) {
-                    Value = String.Empty;
-                }
-                else {
-                    // we don't need to add the Separator around the string here anymore
-                    Value = string.Join(Separator.ToString(), value);
-                }
-            }
+            get => Value?.Split(new[] { Separator }, StringSplitOptions.RemoveEmptyEntries) ?? new string[0];
+            set => Value = value?.Length > 0 ? string.Join(Separator.ToString(), value) : "";
         }
     }
 }
