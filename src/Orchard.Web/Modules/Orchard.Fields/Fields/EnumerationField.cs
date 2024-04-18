cfc122a67a6ad2c6725921ccdd82b7fc508da676
Matteo Piovanelli <MatteoPiovanelli-Laser@users.noreply.github.com>
2024-04-18 17:39:53 +0200

    Reworking EnumerationField's logic to store/retrieve its (selected) values

--- src/Orchard.Web/Modules/Orchard.Fields/Fields/EnumerationField.cs
+++ src/Orchard.Web/Modules/Orchard.Fields/Fields/EnumerationField.cs
@@ -7,18 +7,23 @@ namespace Orchard.Fields.Fields {
         private const char Separator = ';';
 
         public string Value {
-            get { return Storage.Get<string>(); }
-            set { Storage.Set(value ?? String.Empty); }
+            get { return Storage.Get<string>().Trim(new char[] { Separator }); }
+            set {
+                Storage.Set(string.IsNullOrEmpty(value)
+                    ? string.Empty
+                    // we add the Separator around the value here
+                    : Separator + value.Trim(new char[] { Separator }) + Separator);
+            }
         }
 
         public string[] SelectedValues {
             get {
                 var value = Value;
-                if(string.IsNullOrWhiteSpace(value)) {
+                if (string.IsNullOrWhiteSpace(value)) {
                     return new string[0];
                 }
 
-                return value.Split(new [] { Separator }, StringSplitOptions.RemoveEmptyEntries);
+                return value.Split(new[] { Separator }, StringSplitOptions.RemoveEmptyEntries);
             }
 
             set {
@@ -26,7 +31,8 @@ namespace Orchard.Fields.Fields {
                     Value = String.Empty;
                 }
                 else {
-                    Value = Separator + string.Join(Separator.ToString(), value) + Separator;
+                    // we don't need to add the Separator around the string here anymore
+                    Value = string.Join(Separator.ToString(), value);
                 }
             }
         }
