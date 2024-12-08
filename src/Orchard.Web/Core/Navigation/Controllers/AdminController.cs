e68d25e46d0bda654aa62aee1ae84f3d4098b9a1
Benedek Farkas <benedek.farkas@lombiq.com>
2024-12-08 13:58:44 +0100

    Fixing merge and consolidating NuGet packages

--- src/Orchard.Web/Core/Navigation/Controllers/AdminController.cs
+++ src/Orchard.Web/Core/Navigation/Controllers/AdminController.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
-using System.Web.Routing;
 using Orchard.ContentManagement;
 using Orchard.ContentManagement.Aspects;
 using Orchard.ContentManagement.Handlers;
